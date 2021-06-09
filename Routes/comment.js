445bfb05f601b87c7f5d5db7bdc2f07a809a01de
SarahMabd <sarahmabd@gmail.com>
2021-06-09 13:41:05 +0200

    route commit

---
+++ Routes/comment.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const CommentRouter = express.Router()
+const Posts = require('./../Models/posts')
+
+//add new comment
+CommentRouter.post('/addComment/:postId', async (req, res) => {
+    const { commenter, comment } = req.body;
+    let addComment;
+    try {
+        const post = await Posts.findOne({ _id: req.params.postId }).exec();
+        if (post != null) {
+            addComment = { commenter, comment };
+            await Posts.updateOne({ _id: req.params.postId }, { $push: { comments: addComment } });
+            res.statusCode = 200;
+            res.send({ "message": "Comment added successfully" });
+        } else {
+            res.statusCode = 404;
+            res.send({ "message": "There is no post like that" });
+        }
+    } catch (err) {
+        res.statusCode = 422;
+        res.send({ "message": "Something went wrong, try to refresh" });
+    }
+});
+
+//get list of all comment on a post by id
+CommentRouter.get('/comments/:postId', async (req, res) => {
+    try {
+        const post = await Posts.findOne({ _id: req.params.postId }).exec();
+        if (post != null) {
+            res.statusCode = 200;
+            res.send({ "Comments": post.comments });
+        } else {
+            res.statusCode = 404;
+            res.send({ "message": "There is no post like that" });
+        }
+    } catch (err) {
+        res.statusCode = 422;
+        res.send({ "message": "Something went wrong, try to refresh" });
+    }
+});
+
+
+
+
+
+module.exports = CommentRouter;
